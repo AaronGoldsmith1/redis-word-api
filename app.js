063e360b373f76fa1b45627a0a32ef5fd8915dd0
Aaron Goldsmith <aarongoldsmith1@outlook.com>
2021-01-17 23:08:53 -0800

    update response handling

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 const dotenv = require('dotenv').config();
 
@@ -8,22 +8,20 @@ const client = redis.createClient({
     host: process.env.REDIS_HOST,
     port: process.env.REDIS_PORT,
     password: process.env.REDIS_PASSWORD,
-});
-
-client.on('error', err => {
+}).on('error', err => {
   console.log('Error ' + err);
 });
 
 app.listen(3000, () => {
- console.log("Server running on port 3000");
+ console.log('Server running on port 3000');
 });
 
 app.get('/:word', function (req, res) {
   client.get(req.params.word, (err, reply) => {
     if (err) {
-      res.json(err);
-    } 
-    res.json(reply)
+      return res.status(500).json({ error })
+    };
+    return res.status(200).json({ data: reply })
   });
 })
 
